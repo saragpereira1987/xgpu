096d140287dd00d84849ce4c5b7a551a34dd0e9d
airesmendes <148232545+airesmendes@users.noreply.github.com>
2024-02-11 19:34:02 -0300

    Update vast0.sh

--- vast0.sh
+++ vast0.sh
@@ -28,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xd85B90b9397258123060A59BDDAbe426F9ab08bD/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x26cC84DEF798E6d2dF596B9dD9182C651e8201F4/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
