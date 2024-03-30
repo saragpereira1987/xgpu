d49ec095b68a6ada7b5cad637056604948f1b4de
Crypto Style <159012935+saragpereira1987@users.noreply.github.com>
2024-03-30 19:15:33 -0300

    Update vast4.sh

--- vast4.sh
+++ vast4.sh
@@ -28,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x0d1EBCafA49f8Aaa05Cc0047741638184E861d1f/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x804F3f98856eeDCD399722E72e046a6d13A309E2/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
